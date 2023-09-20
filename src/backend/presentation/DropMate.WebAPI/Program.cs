5b9efb606b7ee6b32addcae2054cb912a429bd52
Aliyu Abdullahi <88856370+Aliyu-code-portfolio@users.noreply.github.com>
2023-09-20 06:37:50 +0100

    Update Program.cs Logger

--- src/backend/presentation/DropMate.WebAPI/Program.cs
+++ src/backend/presentation/DropMate.WebAPI/Program.cs
@@ -5,8 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
-LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
-"/nlog.config"));
+//LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+//"/nlog.config"));
 
 
 // Add services to the container.
